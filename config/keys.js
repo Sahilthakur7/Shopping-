99c191cea4e872ce4f5cd95282071a279340f7b4
SahilThakur7 <sahilrd07@gmail.com>
2019-02-25 09:39:32 +0530

    mongo connected to cloud database

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = { 
+    mongoURI: 'mongodb://KV:1515151515@cluster0-shard-00-00-nxh60.mongodb.net:27017,cluster0-shard-00-01-nxh60.mongodb.net:27017,cluster0-shard-00-02-nxh60.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+}
--- server.js
+++ server.js
@@ -2,16 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/ShoppingList', {useNewUrlParser: true});
+//MongoConnection
+
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db).then(() => console.log("Connection to mongo established")).catch(err => console.log(err));
+
+//Use Routes
+
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5000 ;
 
-mongoose.connection.once('open',function(){
-    console.log('Connection made,check port 5000');
-}).on('error',function(error){
-    console.log('Connection broken',error);
-})
+app.listen(port, () => console.log('Server started on port', port));
+
