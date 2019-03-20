6b79d72038bfd99d2e6a99c68f2ac248b3c7627c
SahilThakur7 <sahilrd07@gmail.com>
2019-03-20 11:46:45 +0530

    finished with deleting and adding item using redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,12 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import store from './store';
+import { Container } from 'reactstrap';
 
 
 import Navbar from './components/navbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/itemModal';
 
 
 class App extends Component {
@@ -16,7 +18,10 @@ class App extends Component {
             <Provider store={store}>
                 <div className="App">
                     <Navbar />
-                    <ShoppingList />
+                    <Container>
+                        <ItemModal />
+                        <ShoppingList />
+                    </Container>
                 </div>
             </Provider>
         );
